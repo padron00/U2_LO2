4cbd4eacad9f2a2eaa4b49cec8d01a459d2cf44f
padron00 <enriquepadrongmz@gmail.com>
2022-03-03 19:18:31 -0600

    first try

---
+++ sw.js
@@ -0,0 +1,60 @@
+var cacheName = 'weatherPWA-v1';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/scripts/app.js',
+    '/scripts/localforage-1.4.0.js',
+    '/styles/ud811.css',
+    '/images/clear.png',
+    '/images/cloudy-scattered-showers.png',
+    '/images/cloudy.png',
+    '/images/fog.png',
+    '/images/ic_add_white_24px.svg',
+    '/images/ic_refresh_white_24px.svg',
+    '/images/partly-cloudy.png',
+    '/images/rain.png',
+    '/images/scattered-showers.png',
+    '/images/sleet.png',
+    '/images/snow.png',
+    '/images/thunderstorm.png',
+    '/images/wind.png'
+  ];
+
+  //Instala el service worker y crea un archivo .log, espera hasta que se cierre el appshell
+  // Guarda todos los elementos del appshell como cache
+  self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+
+  // Comprueba si hay cambios en el service worker, reemplazandolo por el nuevo
+  // Remueve la cache antigua
+  self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil (
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+    );
+  });
+
+  // Busca en network
+  // Si la app necesita algo de internet, lo busca en el service worker y lo retorna si es que lo tiene
+  self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  });
